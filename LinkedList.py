d1a4b373a801c601f280ef64e6fcd60a26acbb37
abs007 <53483248+abs007@users.noreply.github.com>
2021-08-31 17:32:22 +0530

    Update and rename LinkedList to LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -39,3 +39,25 @@ class List:
             else:
                 curr_node=curr_node.next
         
+        
+        
+       ### Example test cases ###
+        
+    l1=List()
+    l1.head=node('1')
+    l1.tail=node('2')
+    l1.head.next=l1.tail
+    l1.display()
+ >>>['1', '2']
+ 
+    l1.append('3')
+    l1.append('4')
+    l1.display()
+ >>>['1', '2', '3', '4']
+ 
+    l1.appbeg('0')
+    l1.display()
+ >>>['0', '1', '2', '3', '4']
+
+    l1.add('4','5')             ##This func doesn't need display() because 'return self.display() is already present in the while loop
+ >>>['0', '1', '2', '3', '4', '5']
